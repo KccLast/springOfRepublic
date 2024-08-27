c636a7b42c6a7fe127af278bfcd2a05c4ce22c36
mimi5963 <mimi5963@naver.com>
2024-08-28 00:01:45 +0900

    init: 프로젝트 셋팅 JSP 의존성 추가

---
+++ src/main/java/com/kcc/rich/JspTestController.java
@@ -0,0 +1,12 @@
+package com.kcc.rich;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class JspTestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";  // home.jsp를 가리킴
+    }
+}
